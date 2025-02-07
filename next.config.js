05ef97ee9126131a9a03d897faad23661c4003ac
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2025-02-06 20:46:26 -0500

    test

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // Add the following line to the next.config.js file to resolve the error:
-  webpack5: false,
 };
 
 module.exports = nextConfig;
