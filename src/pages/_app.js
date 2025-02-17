c3b7fd8dd5d33fa2b53ffd446a400d363c17a5c9
Pickworth <spckworth1991@gmail.com>
2025-02-17 11:38:54 -0500

    favico

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,10 @@ import React from "react"; // Import React
 
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
+
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
   return <Component {...pageProps} />;
 }
 
